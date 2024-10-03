32491cf38d6e5b9727cf8651f6df5fbc8a443bc6
jfourty <thedailybank1111@gmail.com>
2024-10-03 13:55:23 -0400

    progress bar to find money

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var money = 0;
 var scavs = 0;
+var isFindingMoney = false;
+var progressValue = 0;
+var progressInterval;
 
 function updateTitle() {
     document.title = "$" + round(money);
@@ -47,6 +50,41 @@ function load(){
 
 function remove(){
     localStorage.removeItem("save")
+}
+
+function toggleProgress() {
+    isFindingMoney = !isFindingMoney;
+    var progressBar = document.getElementById("moneyProgress");
+    var toggleButton = document.getElementById("toggleProgress");
+
+    if (isFindingMoney) {
+        progressValue = 0;
+        progressBar.value = progressValue;
+        progressBar.style.display = "inline";
+        toggleButton.innerText = "stop finding money";
+        startProgress();
+    } else {
+        stopProgress();
+        progressBar.style.display = "none";
+        toggleButton.innerText = "start finding money";
+    }
+}
+
+function startProgress() {
+    progressInterval = setInterval(function() {
+        if (progressValue < 100) {
+            progressValue += 1; // Increment progress
+            document.getElementById("moneyProgress").value = progressValue;
+        } else {
+            money += 10; // Reward for completing progress
+            document.getElementById("money").innerHTML = money;
+            progressValue = 0; // Reset progress
+        }
+    }, 100); // Update every 100ms
+}
+
+function stopProgress() {
+    clearInterval(progressInterval);
 }
 
 window.setInterval(function(){
