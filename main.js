cd1127c9be88ca8f8f68853b53baa27159e0fb69
40urty <thedailybank1111@gmail.com>
2024-10-02 22:01:26 -0400

    added a buyable, gameloop, and renamed findClick to moneyClick

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var money = 0;
+var scavs = 0;
 
-function findClick(number){
+function moneyClick(number){
     money = money + number;
     document.getElementById("money").innerHTML = money;
 };
+
+function buyScav(){
+    var scavsCost = Math.floor(100 * Math.pow(1.15,scavs));
+    if(money >= scavsCost){
+        scavs = scavs + 1;
+    	money = money - scavsCost;
+        document.getElementById('scavs').innerHTML = scavs;
+        document.getElementById('money').innerHTML = money;
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.15,scavs));
+    document.getElementById('scavsCost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+    moneyClick(scavs);
+}, 1000);
